c44098f6df95667739df0d2f8193b3ccf4723283
Simone3 <simone.graziussi@gmail.com>
2016-05-20 14:20:44 +0200

    Dependencies and gradle setup

---
+++ lifecycle_lint_checks/src/main/java/it/polimi/testing/lifecycle_lint/registry/LifecycleIssuesRegistry.java
@@ -0,0 +1,15 @@
+package it.polimi.testing.lifecycle_lint.registry;
+
+import com.android.tools.lint.client.api.IssueRegistry;
+import com.android.tools.lint.detector.api.Issue;
+
+import java.util.List;
+
+public class LifecycleIssuesRegistry extends IssueRegistry
+{
+    @Override
+    public List<Issue> getIssues()
+    {
+        return null;
+    }
+}
